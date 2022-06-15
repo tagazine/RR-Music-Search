72b5ff5324a9a6fdafdd4e646cd6b55387582df7
Your Namegit config --global user.name Your <you@example.com>
2022-06-15 17:42:06 -0400

    react routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import Searchbar from "./components/Searchbar";
 import { DataContext } from "./context/DataContext";
 import { SearchContext } from "./context/SearchContext";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
 
 function App() {
   let [message, setMessage] = useState("Search for Music!");
@@ -14,32 +17,45 @@ function App() {
   const handleSearch = (e, term) => {
     e.preventDefault();
     const fetchData = async () => {
-        document.title = `${term} Music`;
-        const response = await fetch(API_URL + term);
-        const resData = await response.json();
-        if (resData.results.length > 0) {
-          setData(resData.results);
-        } else {
-          setMessage("Not Found");
-        }
-      };
-      fetchData();
+      document.title = `${term} Music`;
+      const response = await fetch(API_URL + term);
+      const resData = await response.json();
+      if (resData.results.length > 0) {
+        setData(resData.results);
+      } else {
+        setMessage("Not Found");
+      }
+    };
+    fetchData();
   };
 
   return (
     <div className="App">
-      <SearchContext.Provider
-        value={{
-          term: searchInput,
-          handleSearch: handleSearch,
-        }}
-      >
-        <Searchbar />
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <React.Fragment>
+                <SearchContext.Provider
+                  value={{
+                    term: searchInput,
+                    handleSearch: handleSearch,
+                  }}
+                >
+                  <Searchbar />
+                </SearchContext.Provider>
+                <DataContext.Provider value={data}>
+                  <Gallery />
+                </DataContext.Provider>
+              </React.Fragment>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
